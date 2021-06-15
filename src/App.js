546dcdfee101daf58f834f19130565823bb78c2c
Abhay5855 <ap3919529@gmail.com>
2021-06-15 15:45:58 +0530

    docs: Import rows and Banner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
-
+import "./App.css";
+import Rows from "./components/Rows";
+import requests from "./requests";
+import Banner from "./components/Banner"
 function App() {
   return (
     <div className="App">
+      
+
+
+      <Banner />
+      
+     
+      <Rows
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        //making the image bigger this default to true so no need of = and {}
+        isImageBigger
+      />
+
+      <Rows title="Trending" fetchUrl={requests.fetchTrending } />
+      <Rows title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Rows title="Horror" fetchUrl={requests.fetchHorrorMovies} />
      
     </div>
   );
