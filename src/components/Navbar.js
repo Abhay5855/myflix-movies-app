8ce3c53d10399228047ade5082a96e00bf262e70
Abhay5855 <ap3919529@gmail.com>
2021-06-15 15:45:23 +0530

    docs: Requests of API

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar__container">
+
+    
+      <div className="navbar__input">
+        <input placeholder="Search..." />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/requests.js
@@ -0,0 +1,20 @@
+const API_KEY = "d97b3e0257a2c71f178ef69c4ca4f27c"
+
+
+
+const requests = {
+
+
+  fetchTrending: `trending/all/day?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  
+  
+
+
+}
+
+
+export default requests;
